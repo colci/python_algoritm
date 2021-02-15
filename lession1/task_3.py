b8dd677e628532f5764822055f27fb1ec81c6f34
Alexander <colci@yandex.ru>
2021-02-15 15:35:50 +0300

    Заглушка, в течении пару дней доделаю.

--- lession1/task_3.py
+++ lession1/task_3.py
@@ -23,6 +23,8 @@
 """
 
 
+# *******************************************  Вариант №1 *************************************************************
+# O(n log n)
 def get_top_profit_company(top, dct_company):
     list_company = list(dct_company.items())
     list_company.sort(key=lambda i: i[1], reverse=True)
@@ -31,8 +33,27 @@ def get_top_profit_company(top, dct_company):
 
 company_annual_profit = {'company 1': 70000000, 'company 2': 20000000,
                          'company 3': 40000000, 'company 5': 100000000,
-                         'company 6': 850000000}
+                         'company 6': 850000000, 'company 7': 100000}
 
 top_list_company_profit = get_top_profit_company(3, company_annual_profit)
 for i in top_list_company_profit:
     print(i[0], ':', i[1])
+
+lst_company_profit = list(company_annual_profit.items())
+
+
+# ******************************** Вариант №2 ************************************************************************
+# О(n2)
+def get_top_profit_company2(top, lst_company):
+    for i in range(len(lst_company)):
+        for j in range(len(lst_company) - 1):
+            if lst_company[j + 1][1] > lst_company[j][1]:
+                lst_company[j + 1], lst_company[j] = lst_company[j], lst_company[j + 1]
+    return lst_company[:top]
+
+
+for i in get_top_profit_company2(3, lst_company_profit):
+    print(i[0], ':', i[1])
+
+
+"""первым способом более эффективен так как О-нотация логарифмическая """
